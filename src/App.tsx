4e5b55c19f8c51107471123e89337f7d45a3eebf
leesm0218 <leesm0218@naver.com>
2025-06-02 17:36:16 +0900

    feat: routes 기본 코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { Routes, Route } from 'react-router-dom';
 import { NavigationBar } from './components/NavigationBar';
+import { ROUTEURL } from './config/routes';
+
+function SubscriptionPage() {
+  return <div>구독 페이지</div>;
+}
+function RecordingPage() {
+  return <div>녹화목록 페이지</div>;
+}
+function NotificationPage() {
+  return <div>알림 페이지</div>;
+}
+function ProfilePage() {
+  return <div>마이 페이지</div>;
+}
 
 function App() {
-  return <NavigationBar />;
+  return (
+    <>
+      <Routes>
+        <Route path={ROUTEURL.SUBSCRIPTION} element={<SubscriptionPage />} />
+        <Route path={ROUTEURL.RECORDING} element={<RecordingPage />} />
+        <Route path={ROUTEURL.NOTIFICATION} element={<NotificationPage />} />
+        <Route path={ROUTEURL.PROFILE} element={<ProfilePage />} />
+      </Routes>
+      <NavigationBar />
+    </>
+  );
 }
 
 export default App;
