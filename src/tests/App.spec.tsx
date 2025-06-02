dcf4909cf454f429b0b6e3bebb5b8aa17e3a3ad0
leesm0218 <leesm0218@naver.com>
2025-06-02 20:44:48 +0900

    feat: 정적 테스트 코드 작성

---
+++ src/tests/App.spec.tsx
@@ -0,0 +1,49 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+import { describe, it, expect } from 'vitest';
+import { ROUTEURL } from '../config/routes.ts';
+import App from '../App';
+
+describe('App', () => {
+  it('첫 화면이 subscription URL 화면인지 확인', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('구독 페이지 입니다')).toBeInTheDocument();
+  });
+  it('구독 페이지가 정상적으로 출력되는지 확인', () => {
+    render(
+      <MemoryRouter initialEntries={[ROUTEURL.SUBSCRIPTION]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('구독 페이지 입니다')).toBeInTheDocument();
+  });
+  it('녹화목록 페이지가 정상적으로 출력되는지 확인', () => {
+    render(
+      <MemoryRouter initialEntries={[ROUTEURL.RECORDING]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('녹화목록 페이지 입니다')).toBeInTheDocument();
+  });
+  it('알림 페이지가 정상적으로 출력되는지 확인', () => {
+    render(
+      <MemoryRouter initialEntries={[ROUTEURL.NOTIFICATION]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('알림 페이지 입니다')).toBeInTheDocument();
+  });
+  it('마이 페이지가 정상적으로 출력되는지 확인', () => {
+    render(
+      <MemoryRouter initialEntries={[ROUTEURL.PROFILE]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getAllByText('마이 페이지 입니다')[0]).toBeInTheDocument();
+  });
+});
--- src/tests/sample.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, test, expect } from 'vitest';
-
-describe('sample.spec.ts', () => {
-  test('test', () => {
-    expect(1 + 1).toBe(2);
-  });
-});
