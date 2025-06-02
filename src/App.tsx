fccde0b0989e3904b4e0f4038e34f3c48382fabc
leesm0218 <leesm0218@naver.com>
2025-06-02 20:57:42 +0900

    feat: 클릭 이벤트 테스트 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,23 @@ import { NavigationBar } from './components/NavigationBar';
 import { ROUTEURL } from './config/routes';
 
 function SubscriptionPage() {
-  return <div>구독 페이지</div>;
+  return <div>구독 페이지 입니다</div>;
 }
 function RecordingPage() {
-  return <div>녹화목록 페이지</div>;
+  return <div>녹화목록 페이지 입니다</div>;
 }
 function NotificationPage() {
-  return <div>알림 페이지</div>;
+  return <div>알림 페이지 입니다</div>;
 }
 function ProfilePage() {
-  return <div>마이 페이지</div>;
+  return <div>마이 페이지 입니다</div>;
 }
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<SubscriptionPage />} />
         <Route path={ROUTEURL.SUBSCRIPTION} element={<SubscriptionPage />} />
         <Route path={ROUTEURL.RECORDING} element={<RecordingPage />} />
         <Route path={ROUTEURL.NOTIFICATION} element={<NotificationPage />} />
--- src/tests/App.spec.tsx
+++ src/tests/App.spec.tsx
@@ -1,9 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
 import { describe, it, expect } from 'vitest';
 import { ROUTEURL } from '../config/routes.ts';
 import App from '../App';
+
+const user = userEvent.setup();
 
 describe('App', () => {
   it('첫 화면이 subscription URL 화면인지 확인', () => {
@@ -14,36 +17,74 @@ describe('App', () => {
     );
     expect(screen.getByText('구독 페이지 입니다')).toBeInTheDocument();
   });
-  it('구독 페이지가 정상적으로 출력되는지 확인', () => {
+  it('구독 페이지가 정상적으로 출력되는지 확인', async () => {
     render(
       <MemoryRouter initialEntries={[ROUTEURL.SUBSCRIPTION]}>
         <App />
       </MemoryRouter>
     );
     expect(screen.getByText('구독 페이지 입니다')).toBeInTheDocument();
+
+    await user.click(screen.getByText('녹화목록'));
+    expect(screen.getByText('녹화목록 페이지 입니다')).toBeInTheDocument();
+
+    // 네비게이션 바에서 "알림" 버튼 클릭
+    await user.click(screen.getByText('알림'));
+    expect(screen.getByText('알림 페이지 입니다')).toBeInTheDocument();
+
+    // 네비게이션 바에서 "마이 페이지" 버튼 클릭
+    await user.click(screen.getByText('마이 페이지'));
+    expect(screen.getByText('마이 페이지 입니다')).toBeInTheDocument();
   });
-  it('녹화목록 페이지가 정상적으로 출력되는지 확인', () => {
+  it('녹화목록 페이지가 정상적으로 출력되는지 확인', async () => {
     render(
       <MemoryRouter initialEntries={[ROUTEURL.RECORDING]}>
         <App />
       </MemoryRouter>
     );
     expect(screen.getByText('녹화목록 페이지 입니다')).toBeInTheDocument();
+
+    await user.click(screen.getByText('구독'));
+    expect(screen.getByText('구독 페이지 입니다')).toBeInTheDocument();
+
+    await user.click(screen.getByText('알림'));
+    expect(screen.getByText('알림 페이지 입니다')).toBeInTheDocument();
+
+    await user.click(screen.getByText('마이 페이지'));
+    expect(screen.getByText('마이 페이지 입니다')).toBeInTheDocument();
   });
-  it('알림 페이지가 정상적으로 출력되는지 확인', () => {
+  it('알림 페이지가 정상적으로 출력되는지 확인', async () => {
     render(
       <MemoryRouter initialEntries={[ROUTEURL.NOTIFICATION]}>
         <App />
       </MemoryRouter>
     );
     expect(screen.getByText('알림 페이지 입니다')).toBeInTheDocument();
+
+    await user.click(screen.getByText('구독'));
+    expect(screen.getByText('구독 페이지 입니다')).toBeInTheDocument();
+
+    await user.click(screen.getByText('녹화목록'));
+    expect(screen.getByText('녹화목록 페이지 입니다')).toBeInTheDocument();
+
+    await user.click(screen.getByText('마이 페이지'));
+    expect(screen.getByText('마이 페이지 입니다')).toBeInTheDocument();
   });
-  it('마이 페이지가 정상적으로 출력되는지 확인', () => {
+  it('마이 페이지가 정상적으로 출력되는지 확인', async () => {
     render(
       <MemoryRouter initialEntries={[ROUTEURL.PROFILE]}>
         <App />
       </MemoryRouter>
     );
     expect(screen.getAllByText('마이 페이지 입니다')[0]).toBeInTheDocument();
+
+    await user.click(screen.getByText('구독'));
+    expect(screen.getByText('구독 페이지 입니다')).toBeInTheDocument();
+
+    await user.click(screen.getByText('녹화목록'));
+    expect(screen.getByText('녹화목록 페이지 입니다')).toBeInTheDocument();
+
+    await user.click(screen.getByText('알림'));
+    expect(screen.getByText('알림 페이지 입니다')).toBeInTheDocument();
   });
 });
