0c69318efe149fbfcdfca3922c882b4caab9103b
leesm0218 <leesm0218@naver.com>
2025-06-02 16:55:03 +0900

    style: Prettier 적용.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -10,18 +10,21 @@ const TYPES = [
   'test',
   'chore',
   'rename',
-  'remove'
+  'remove',
 ];
 
 module.exports = {
   plugins: [
     {
       rules: {
-        'commit_message_pattern': ({raw}) => {
+        commit_message_pattern: ({ raw }) => {
           const messageType = raw.split(': ')[0];
           const checkResult = TYPES.includes(messageType);
           if (!checkResult) {
-            return [false, '커밋 메시지는 지정된 타입: 내용 이어야 합니다. 타입명, :뒤에 공백 한칸을 체크해주세요'];
+            return [
+              false,
+              '커밋 메시지는 지정된 타입: 내용 이어야 합니다. 타입명, :뒤에 공백 한칸을 체크해주세요',
+            ];
           }
           return [true, ''];
         },
@@ -29,7 +32,17 @@ module.exports = {
     },
   ],
   rules: {
-    'commit_message_pattern': [2, 'always'],
-    'subject-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case']]
+    commit_message_pattern: [2, 'always'],
+    'subject-case': [
+      2,
+      'always',
+      [
+        'sentence-case',
+        'start-case',
+        'pascal-case',
+        'upper-case',
+        'lower-case',
+      ],
+    ],
   },
 };
