bae8fa34bdefd5ec92b135d73430d79c01fed1e1
leesm0218 <leesm0218@naver.com>
2025-06-05 19:59:35 +0900

    feat: 백엔드와 연동되는 기본 동적 코드 작성

--- src/pages/Home/Notification.tsx
+++ src/pages/Home/Notification.tsx
@@ -1,11 +1,42 @@
 import { NOTIFICATION_STYLE as style } from '@/constants/styles';
 import { NotificationItem } from '@/components/ListItem';
-import { notificationItemDatas } from '@/constants/notification/notificationConstants';
+import { useQuery } from '@tanstack/react-query';
+import type { NotificationItemProps } from '@/types/notification/notificationTypes';
+
+const notificationURL = import.meta.env.VITE_API_BASE_URL + 'notifications';
+
+const fetchNotifications = async () => {
+  const res = await fetch(notificationURL, {
+    headers: {},
+  });
+  if (!res.ok) {
+    throw new Error('알림 정보를 불러올 수 없습니다.');
+  }
+  const json = await res.json();
+  return json.data;
+};
 
 export const NotificationPage = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: [''],
+    queryFn: () => fetchNotifications(),
+  });
+
+  if (isLoading) {
+    return <div>로딩 중</div>;
+  }
+
+  if (error) {
+    return <div>오류 발생</div>;
+  }
+
+  if (!data) {
+    return <div>오류 발생</div>;
+  }
+
   return (
     <div className={style.ITEM_LIST}>
-      {notificationItemDatas.map((props) => (
+      {data.notifications.map((props: NotificationItemProps) => (
         <NotificationItem key={props.notificationId} {...props} />
       ))}
     </div>
