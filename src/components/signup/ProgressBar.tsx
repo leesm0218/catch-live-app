7ed7e3640cdf7cfc4aef0dbd084fd8256cc235cd
soo <dlgustn96@gmail.com>
2025-06-11 13:16:59 +0900

    refactor: ProgressBar에 half/full 구분을 위한 progress prop 추가

--- src/components/signup/ProgressBar.tsx
+++ src/components/signup/ProgressBar.tsx
@@ -1,9 +1,19 @@
 import { SIGNUP_PAGE_STYLE } from '@/constants/styles';
 
-const ProgressBar = () => {
+type ProgressType = 'half' | 'full';
+
+interface ProgressBarType {
+  progress: ProgressType;
+}
+
+const ProgressBar = ({ progress }: ProgressBarType) => {
+  const progressBarStyle =
+    progress === 'full'
+      ? SIGNUP_PAGE_STYLE.progressBarComplete
+      : SIGNUP_PAGE_STYLE.progressBar;
   return (
     <div className={SIGNUP_PAGE_STYLE.progressBarbox}>
-      <div className={SIGNUP_PAGE_STYLE.progressBarComplete}></div>
+      <div className={progressBarStyle}></div>
     </div>
   );
 };
