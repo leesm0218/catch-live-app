1a2f8ad86dc318ce021b7cb525fd69ac1b55c92d
leesm0218 <leesm0218@naver.com>
2025-06-17 15:25:42 +0900

    feat: 쿠키와 로컬스토리지 제거 기능 추가

--- src/components/Logout/LogoutButton.tsx
+++ src/components/Logout/LogoutButton.tsx
@@ -3,12 +3,14 @@ import { PROFILE_STYLE as style } from '@/constants/styles';
 import { useNavigate } from 'react-router-dom';
 import { ROUTE_URL_FULL } from '@/constants/routers';
 import { useCallback } from 'react';
+import { clearCookie } from '@/utils/clearCookie';
 
 export function LogOutButton() {
   const navigate = useNavigate();
 
   const handleLogout = useCallback(async () => {
     await fetchLogout();
+    clearCookie();
     navigate(ROUTE_URL_FULL.LOGIN);
   }, [navigate]);
 
--- src/components/Signout/SignoutButton.tsx
+++ src/components/Signout/SignoutButton.tsx
@@ -3,12 +3,16 @@ import { PROFILE_STYLE as style } from '@/constants/styles';
 import { useNavigate } from 'react-router-dom';
 import { ROUTE_URL_FULL } from '@/constants/routers';
 import { useCallback } from 'react';
+import { clearLocalStorage } from '@/utils/clearLocalStorage';
+import { clearCookie } from '@/utils/clearCookie';
 
 export function SignoutButton() {
   const navigate = useNavigate();
 
   const handleLogout = useCallback(async () => {
     await fetchSignOut();
+    clearCookie();
+    clearLocalStorage();
     navigate(ROUTE_URL_FULL.LOGIN);
   }, [navigate]);
 
