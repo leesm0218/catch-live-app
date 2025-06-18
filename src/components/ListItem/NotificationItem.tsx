1e44572e95d250a775ec98af6e5d0b1e36d852c4
gt.park <smileboy0014@gmail.com>
2025-06-18 09:55:25 +0900

    refactor: 알림 조회 페이지 무한 스크롤 로직 수정

--- src/components/ListItem/NotificationItem.tsx
+++ src/components/ListItem/NotificationItem.tsx
@@ -1,4 +1,5 @@
 import { NOTIFICATION_STYLE as style } from '@/constants/styles';
+import { toKstDate } from '@/utils/dateUtils';
 import type { NotificationItemViewProps } from '@/types/notificationTypes';
 
 export const NotificationItem = (props: NotificationItemViewProps) => {
@@ -6,20 +7,8 @@ export const NotificationItem = (props: NotificationItemViewProps) => {
 
   return (
     <div className={style.item}>
-      <div className={style.item_info}>{content}</div>
-      <div className={style.item_time}>
-        {formatDate(new Date(createdAt).toISOString())}
-      </div>
+      <div className={style.itemInfo}>{content}</div>
+      <div className={style.itemTime}>{toKstDate(createdAt)}</div>
     </div>
   );
 };
-
-function formatDate(dateString: string) {
-  const date = new Date(dateString);
-  const yyyy = date.getFullYear();
-  const mm = String(date.getMonth() + 1).padStart(2, '0');
-  const dd = String(date.getDate()).padStart(2, '0');
-  const hh = String(date.getHours()).padStart(2, '0');
-  const min = String(date.getMinutes()).padStart(2, '0');
-  return `${yyyy}.${mm}.${dd} ${hh}:${min}`;
-}
