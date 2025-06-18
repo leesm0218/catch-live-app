9656e2e1bf23d4c89f9456ff2d1e3ae59226832f
gt.park <smileboy0014@gmail.com>
2025-06-18 09:54:14 +0900

    chore: 전역에 queryClient 옵션 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryClient,
+  QueryClientProvider,
+  type QueryClientConfig,
+} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import GlobalErrorBoundary from './pages/Error/GlobalErrorBoundary.tsx';
 import '@/index.css';
 import App from '@/App.tsx';
+import { API_RETRY_COUNT, API_STALE_TIME } from './constants/api.ts';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      retry: API_RETRY_COUNT,
+      staleTime: API_STALE_TIME,
+      refetchOnWindowFocus: true,
+      refetchOnMount: true,
+    },
+  },
+} as QueryClientConfig);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
